e129d15c088d38ff632875b32d5ae40e03c5dcbf
Jochem Smit <jhsmit@gmail.com>
2020-11-03 16:49:31 +0100

    travis + test updates

--- tests/test_hdxrate.py
+++ tests/test_hdxrate.py
@@ -6,7 +6,7 @@ from hdxrate.psx.api import calc_k_int as psx_k_int
 from hdxrate import k_int_from_sequence
 
 
-class TestHdxrate(object):
+class TestHDXrate(object):
     """Tests for `hdxrate` package."""
 
     @classmethod
