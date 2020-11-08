0fad87e82b103082d6e40f28cf33c776d2128f32
Jochem Smit <jhsmit@gmail.com>
2020-11-08 19:38:53 +0100

    added license file headers

--- hdxrate/expfact/api.py
+++ hdxrate/expfact/api.py
@@ -1,3 +1,23 @@
+"""
+This script provides and API to expfact intrinsic exchange rate calculation
+Copyright (C) 2020 Jochem Smit
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+"""
+
+
 from hdxrate.expfact.kint import calculate_kint_per_residue
 import numpy as np
 
--- hdxrate/hdxrate.py
+++ hdxrate/hdxrate.py
@@ -1,4 +1,23 @@
-"""Main module."""
+"""
+This script provides and API to expfact and psx intrinsic exchange rate calculation
+Copyright (C) 2020 Jochem Smit
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+"""
+
+
 
 from hdxrate.expfact.api import calc_k_int as expfact_k_int
 from hdxrate.psx.api import calc_k_int as psx_k_int
