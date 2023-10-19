f54d75b9818b68dd8b95fe85d62ce61ada7a5916
Jochem Smit <jhsmit@gmail.com>
2023-10-19 15:42:45 +0200

    bump version

--- hdxrate/__init__.py
+++ hdxrate/__init__.py
@@ -1,7 +1,7 @@
 """Top-level package for HDXrate."""
 
 __author__ = """Jochem Smit"""
-__email__ = 'jhsmit@gmail.com'
-__version__ = '0.2.0'
+__email__ = "jhsmit@gmail.com"
+__version__ = "0.2.2"
 
 from .hdxrate import k_int_from_sequence
